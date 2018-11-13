30c06c476274ac8c015eea16e4bfcd4869d6abbe
Jeffry <jeffryjulianto7@gmail.com>
2018-11-13 21:22:38 +0700

    Initial commit

---
+++ classes/Line_Apps.php
@@ -0,0 +1,45 @@
+<?php
+	class Line_Apps{
+		
+		var $session;
+		var $data;
+		var $bot;
+		
+		function Line_Apps($profile, $bot = null){
+			$this->bot = $bot;
+			$this->profile = $profile;
+			$this->session = new Session($profile->id);
+			$data = $this->session->get('data');
+			if(!$data){
+				$this->init();
+			}else{
+				$this->data = unserialize($this->session->get('data'));
+			}
+		}
+		
+		protected function init(){
+			$this->save();
+		}
+		
+		protected function save(){
+			$this->session->set('data', serialize($this->data));
+		}
+		
+		function on_follow(){
+			
+		}
+		
+		function on_message($text){
+			
+		}
+		
+		function on_postback($data){
+			
+		}
+		
+		function on_unfollow(){
+			$db = new Database();
+			$db->query('UPDATE profiles SET `following` = 0 WHERE id = ' . $this->profile->id);
+			$db->close();
+		}
+	}
