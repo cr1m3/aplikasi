767a4011e8635a69b8995e0c738d1dbe9d962334
cr1m3 <jeffryjulianto7@gmail.com>
2018-11-13 21:57:41 +0700

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+	
+	define('DB_HOST','us-cdbr-iron-east-01.cleardb.net');
+	define('DB_USER','b8efa6c4c7019e');
+	define('DB_PASSWD','b002a9e2');
+	define('DB_DATABASE','heroku_ce867af4a1f1cfa');	
